b021f4d05a7d70c4ee77261ef1dc7c33a22a9070
ErkhanDV <erhan.denis@list.ru>
2022-04-28 23:58:41 +0500

    feat: add content for our pets block

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,2 +1,8 @@
 'use strict'
 
+window.addEventListener('load', (evt) => scrollTo());
+
+function scrollTo() {
+  const btn = document.querySelector("#make-friend-btn");
+  btn.addEventListener('click', () => document.location="#our-pets");
+}
